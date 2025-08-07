3270e490fcd87c957e90f28fea228f93a7eee963
LoganSimba <156838338+LoganSimba@users.noreply.github.com>
2025-08-07 12:08:39 -0400

    main.py

--- main.py
+++ main.py
@@ -186,4 +186,5 @@ async def trends_page(request: Request):
 
 if __name__ == "__main__":
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+
